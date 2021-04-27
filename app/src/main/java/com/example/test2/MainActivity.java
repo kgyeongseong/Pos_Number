c54234188ec60a04dbd0d3891aa685e48e1c988d
kgyeongseong <83118180+kgyeongseong@users.noreply.github.com>
2021-04-28 06:48:15 +0900

    스마트폰 POS 기계화(숫자 버전) 구현

--- app/src/main/java/com/example/test2/MainActivity.java
+++ app/src/main/java/com/example/test2/MainActivity.java
@@ -1,23 +1,40 @@
 package com.example.test2;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.icu.util.TimeUnit;
+import android.media.ExifInterface;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.SurfaceHolder;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 
+// Google ML Kit Vision API
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.mlkit.vision.common.InputImage;
@@ -25,23 +42,37 @@ import com.google.mlkit.vision.text.Text;
 import com.google.mlkit.vision.text.TextRecognition;
 import com.google.mlkit.vision.text.TextRecognizer;
 
+// Google CameraX API
+import androidx.core.content.FileProvider;
+
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    ImageView imageView;
+    Bitmap bitmap;
+    Bitmap rotatedBitmap;
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    String mCurrentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView textView = (TextView) findViewById(R.id.textView);
-
-        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        textView = (TextView) findViewById(R.id.textView);
+        imageView = (ImageView) findViewById(R.id.imageView);
 
         Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                AssetManager assetManager = getResources().getAssets();
 
+
+                dispatchTakePictureIntent();
+                /* OCR 테스트용
+                //AssetManager assetManager = getResources().getAssets();
                 InputStream is;
                 Bitmap bitmap = null;
                 try {
@@ -49,42 +80,135 @@ public class MainActivity extends AppCompatActivity {
                   bitmap = BitmapFactory.decodeStream(is);
                 } catch (IOException e) {
                     e.printStackTrace();
-                }
-                if (bitmap != null) {
-                    Bitmap resizedBitmap = Bitmap.createScaledBitmap(bitmap, 480, 360, true);
-                    imageView.setImageBitmap(resizedBitmap);
-                    bitmap = resizedBitmap;
-
-                    InputImage image = InputImage.fromBitmap(bitmap, 0);
-                    TextRecognizer recognizer = TextRecognition.getClient();
-                    recognizer.process(image)
-                            .addOnSuccessListener(
-                                    new OnSuccessListener<Text>() {
-                                        @Override
-                                        public void onSuccess(Text texts) {
-                                            // 성공
-                                            List<Text.TextBlock> blocks = texts.getTextBlocks();
-                                            for (int i = 0; i < blocks.size(); i++) {
-                                                List<Text.Line> lines = blocks.get(i).getLines();
-                                                for (int j = 0; j < lines.size(); j++) {
-                                                    List<Text.Element> elements = lines.get(j).getElements();
-                                                    for (int k = 0; k < elements.size(); k++) {
-                                                        textView.append(elements.get(k).getText());
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    })
-                            .addOnFailureListener(
-                                    new OnFailureListener() {
-                                        @Override
-                                        public void onFailure(@NonNull Exception e) {
-                                            // 실패
-                                            e.printStackTrace();
-                                        }
-                                    });
-                }
+                }*/
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+
+            File file = new File(mCurrentPhotoPath);
+            try{
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), Uri.fromFile(file));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            rotatedBitmap = null;
+
+            try{
+                ExifInterface ei = new ExifInterface(mCurrentPhotoPath);
+                int orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_UNDEFINED);
+
+                switch (orientation) {
+                    case ExifInterface.ORIENTATION_ROTATE_90:
+                        rotatedBitmap = rotateImage(bitmap, 90);
+                        break;
+                    case ExifInterface.ORIENTATION_ROTATE_180:
+                        rotatedBitmap = rotateImage(bitmap, 180);
+                        break;
+                    case ExifInterface.ORIENTATION_ROTATE_270:
+                        rotatedBitmap = rotateImage(bitmap, 270);
+                        break;
+                    case ExifInterface.ORIENTATION_NORMAL:
+                    default:
+                        rotatedBitmap = bitmap;
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            imageView.setImageBitmap(rotatedBitmap);
+
+            TextRecognize(rotatedBitmap);
+        }
+    }
+
+    // Text Recognition (텍스트 인식)
+    private void TextRecognize(Bitmap _bitmap) {
+        InputStream is;
+
+        if (_bitmap != null) {
+            Bitmap resizedBitmap = Bitmap.createScaledBitmap(_bitmap, 480, 360, true);
+            imageView.setImageBitmap(resizedBitmap);
+            _bitmap = resizedBitmap;
+
+            InputImage image = InputImage.fromBitmap(_bitmap, 0);
+            TextRecognizer recognizer = TextRecognition.getClient();
+            recognizer.process(image)
+                    .addOnSuccessListener(
+                            new OnSuccessListener<Text>() {
+                                @Override
+                                public void onSuccess(Text texts) {
+                                    // 성공
+                                    textView.setText("");
+                                    List<Text.TextBlock> blocks = texts.getTextBlocks();
+                                    for (int i = 0; i < blocks.size(); i++) {
+                                        List<Text.Line> lines = blocks.get(i).getLines();
+                                        for (int j = 0; j < lines.size(); j++) {
+                                            List<Text.Element> elements = lines.get(j).getElements();
+                                            for (int k = 0; k < elements.size(); k++) {
+                                                textView.append(elements.get(k).getText());
+                                            }
+                                        }
+                                    }
+                                }
+                            })
+                    .addOnFailureListener(
+                            new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    // 실패
+                                    e.printStackTrace();
+                                }
+                            });
+        }
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(Objects.requireNonNull(getApplicationContext()), BuildConfig.APPLICATION_ID + ".provider", photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    public static Bitmap rotateImage(Bitmap source, float angle) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix, true);
     }
 }
